1ac9b04237cf9ed80dc52be9485c152e518e9582
rsj24 <rafi24@gmx.ch>
2019-10-12 15:05:52 +0200

    MLP

--- tensorflow/01_get-started-for-beginners/first-tensorflow-model.py
+++ tensorflow/01_get-started-for-beginners/first-tensorflow-model.py
@@ -10,59 +10,35 @@ mnist = keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
-print("x_train shape", train_images.shape)
-print("y_train shape", train_labels.shape)
+class_names = ['0', '1', '2', '3', '4',
+               '5','6', '7', '8', '9']
+
+train_labels_onehot = keras.utils.to_categorical(train_labels)
+test_labels_onehot = keras.utils.to_categorical(test_labels)
+
 #Build the tf.keras.Sequential model by stacking layers. Choose an optimizer and loss function used for training:
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(100, activation='sigmoid'),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(100, activation='sigmoid'),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(10, activation='sigmoid'),
 ])
 
-#Train and evaluate model:
-# Test accuracy with different loss functions
-# 0.0011 kullback_leibler_divergence
-# 0.0006 poisson
-# 0.9803 sparse_categorical_crossentropy (see for explanation: https://www.reddit.com/r/MLQuestions/comments/93ovkw/what_is_sparse_categorical_crossentropy/)
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+              loss='binary_crossentropy',
               metrics=['accuracy'])
 
-# ---------------------------------------------------------------------------------------------------------------------------------------
-# Train the model
-# ---------------------------------------------------------------------------------------------------------------------------------------
-# Train the model
-# Training the neural network model requires the following steps:
-#
-# 1. Feed the training data to the model. In this example, the training data is in the train_images and train_labels arrays.
-# 2. The model learns to associate images and labels.
-# 3. We ask the model to make predictions about a test set—in this example, the test_images array.
-#    We verify that the predictions match the labels from the test_labels array.
-#
-# To start training, call the model.fit method—so called because it "fits" the model to the training data:
-model.fit(train_images, train_labels, epochs=7)
-
-# ---------------------------------------------------------------------------------------------------------------------------------------
-# Evaluate accuracy
-# ---------------------------------------------------------------------------------------------------------------------------------------
-# Next, compare how the model performs on the test dataset:
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+model.fit(train_images, train_labels_onehot, epochs=1, shuffle=True, batch_size=100)
+test_loss, test_acc = model.evaluate(test_images, test_labels_onehot)
 
 print('\nTest accuracy:', test_acc)
 
-model.summary()
-
-# ---------------------------------------------------------------------------------------------------------------------------------------
-# Make predictions
-# ---------------------------------------------------------------------------------------------------------------------------------------
 predictions = model.predict(test_images)
-print('\nPreditions[0]',predictions[0])
+print('\nPredictions[0]',predictions[0])
 print('Highest confidence value:',np.argmax(predictions[0]))
 
-#
-# Plot the image
-#
 def plot_image(i, predictions_array, true_label, img):
   predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
   plt.grid(False)
@@ -73,13 +49,13 @@ def plot_image(i, predictions_array, true_label, img):
 
   predicted_label = np.argmax(predictions_array)
   if predicted_label == true_label:
-    color = 'blue'
+    color = 'green'
   else:
     color = 'red'
 
-  plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
                                 100*np.max(predictions_array),
-                                true_label),
+                                class_names[true_label]),
                                 color=color)
 
 #
@@ -95,23 +71,49 @@ def plot_value_array(i, predictions_array, true_label):
   predicted_label = np.argmax(predictions_array)
 
   thisplot[predicted_label].set_color('red')
-  thisplot[true_label].set_color('blue')
+  thisplot[true_label].set_color('green')
 
-# Let's test the 10th image
-i=10
+i = 0
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
 plot_image(i, predictions, test_labels, test_images)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions,  test_labels)
 plt.show()
-# predictions = model.predict(test_images)
-# plt.figure(figsize=(10,10))
-# for i in range(25):
-#     plt.subplot(5,5,i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(test_images[i], cmap=plt.cm.binary)
-#     plt.xlabel(predictions[i])
-# plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  test_labels)
+plt.show()
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 10
+num_cols = 10
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, test_labels, test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, test_labels)
+plt.show()
+
+
+# Grab an image from the test dataset.
+img = test_images[0]
+
+print(img.shape)
+
+# Add the image to a batch where it's the only member.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+predictions_single = model.predict(img)
+
+print(predictions_single)
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
